63a95511a83170f2034637b6afb5f149abc137a7
korea8378 <korea8378@naver.com>
2018-07-04 22:42:05 +0900

    Spring junit annotation study

---
+++ SpringStudy2.4.1/src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,87 @@
+package springbook.user.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.domain.User;
+
+//스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능을 지
+@RunWith(SpringJUnit4ClassRunner.class)
+//아래의 에노테이션으로 xml파일을 지정해줌으로써 따로 new를 통해 연결할 필요가 없
+@ContextConfiguration(locations="/applicationContext.xml")
+public class UserDaoTest {
+	//아래의 에노테이션을 통해 getbean을 사용하지 않고도 빈객체를 주입할수 있다
+	@Autowired
+	ApplicationContext context;
+	
+	private UserDao dao; 
+	
+	private User user1;
+	private User user2;
+	private User user3;
+	
+	@Before
+	public void setUp() {
+		this.dao = this.context.getBean("userDao", UserDao.class);
+		
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
+
+	}
+	
+	@Test 
+	public void andAndGet() throws SQLException {		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+	}
+
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
+	}
+
+	
+	@Test
+	public void count() throws SQLException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+				
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+}
