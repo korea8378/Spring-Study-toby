51f16e1ca602562616dd82f2fba130e7ffc50385
korea8378 <korea8378@naver.com>
2018-07-10 22:24:45 +0900

    Spring template/callback study

--- SpringStudy3.5/src/springbook/user/dao/UserDaoTest.java
+++ SpringStudy3.5/src/springbook/user/dao/UserDaoTest.java
@@ -1,10 +1,5 @@
 package springbook.user.dao;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.sql.SQLException;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,8 +9,12 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/test-applicationContext.xml")
